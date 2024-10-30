5e98a55856ae018bfa8a5e2476eb608b5bc6a628
ScryingStan <scryerstan@gmail.com>
2024-10-30 10:48:01 -0400

    Test Update

--- src/main/java/com/Highlighter/HighlighterPlugin.java
+++ src/main/java/com/Highlighter/HighlighterPlugin.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.Highlighter;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "Highlighter Plugin Test 10-30-2024"
 )
-public class ExamplePlugin extends Plugin
+public class HighlighterPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private HighlighterConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	HighlighterConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(HighlighterConfig.class);
 	}
 }
--- src/test/java/com/Highlighter/HighlighterPluginTest.java
+++ src/test/java/com/Highlighter/HighlighterPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.Highlighter;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class HighlighterPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(HighlighterPlugin.class);
 		RuneLite.main(args);
 	}
 }
